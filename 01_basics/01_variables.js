d67cf3faaf0da4cdec93ef2ba5aecefaccedee4a
Jibran Ali <120448199+Jibran25@users.noreply.github.com>
2023-06-19 12:14:51 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 1543
+let email = "jibran1234@gmail.com"
+var passowrd= "12345"
+// accountId= 123456
+
+/*
+    prefer not to use var
+    because issue of block scope and functional scope
+*/
+console.log(accountId)
+console.table([accountId , email, passowrd]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+const name = "jibran"
+let number =  123;
+let logedIn = true
+
+//number  = integer value small size
+//bigint = integer value big size
+//string => ""
+//boolean =>true/false
+//null => standalone value => object
+//undefined => undefined
+
+
+//object
